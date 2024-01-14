f7728db997b2fa22d4e08630a119abfb31ef00e8
CletsyMedia <Cletussam12@yahoo.com>
2024-01-14 17:13:23 +0100

    Updating sql grants

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,14 @@
 -- Create or use the hbnb_dev_db database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- Create or use the hbnb_dev user
+-- Create or use the hbnb_dev user with the specified password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Grant privileges to hbnb_dev user
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
+-- Show grants for the hbnb_dev user
+SHOW GRANTS FOR 'hbnb_dev'@'localhost';
