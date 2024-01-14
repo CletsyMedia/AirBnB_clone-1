dfb46bc213f656b415ea15c0095e8b50b4fd6516
CletsyMedia <Cletussam12@yahoo.com>
2024-01-14 16:55:48 +0100

    using the correct environment variables

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,10 @@
 -- Create or use the hbnb_dev_db database
-CREATE DATABASE IF NOT EXISTS $HBNB_MYSQL_DB;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- Create or use the hbnb_dev user
-CREATE USER IF NOT EXISTS '$HBNB_MYSQL_USER'@'$HBNB_MYSQL_HOST' IDENTIFIED BY '$HBNB_MYSQL_PWD';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant privileges to hbnb_dev user
-GRANT ALL PRIVILEGES ON $HBNB_MYSQL_DB.* TO '$HBNB_MYSQL_USER'@'$HBNB_MYSQL_HOST';
-GRANT SELECT ON performance_schema.* TO '$HBNB_MYSQL_USER'@'$HBNB_MYSQL_HOST';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
