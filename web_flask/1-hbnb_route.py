03d6ac7baf252a88e87eba23cf3f57fffe993ebb
CletsyMedia <Cletussam12@yahoo.com>
2024-02-16 03:17:30 +0100

    Flask web application with two routes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
 """
+This script starts a Flask web application with two routes
+"""
+
 from flask import Flask
 
+# Module name
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
+    """
+    Route that displays "Hello HBNB!"
+    """
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """Displays 'HBNB'."""
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """
+    Route that displays "HBNB"
+    """
     return "HBNB"
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
