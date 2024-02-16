879eb13cd531b675624678383689dfa9e38a30e7
CletsyMedia <Cletussam12@yahoo.com>
2024-02-16 03:34:20 +0100

    Flask web application with three routes

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""Starts a Flask web application with three routes.
 
 The application listens on 0.0.0.0, port 5000.
 Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
+    /: Displays the 'Hello HBNB!'.
+    /hbnb: Displays the 'HBNB'.
+    /c/<text>: Displays the 'C' followed by the value of <text>.
 """
 from flask import Flask
 
@@ -14,22 +14,29 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
+    """
+    Route that displays 'Hello HBNB!'
+    """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'."""
+    """
+    Route that displays 'HBNB'
+    """
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Displays 'C' followed by the value of <text>."""
+def display_c_text(text):
+    """
+    Route that displays 'C' followed by the value of text variable
+    """
+    # Replace underscores with spaces in the text variable
     text = text.replace("_", " ")
-    return "C {}".format(text)
+    return "C " + text
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
