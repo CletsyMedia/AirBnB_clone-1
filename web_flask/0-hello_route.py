6ad55bad5667ba2da52dbdbfdb0e3709fa168a1a
CletsyMedia <Cletussam12@yahoo.com>
2024-02-16 02:25:49 +0100

    Initialization file for the web_flask package

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'
 """
+This script starts a Flask web application with a single route
+"""
+
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -12,9 +11,11 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+    """
+    Route that displays "Hello HBNB!"
+    """
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
